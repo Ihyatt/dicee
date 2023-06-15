ef160184f53207db7a305bda7332c5e678b8c10c
Ihyatt <inas.raheema@gmail.com>
2023-06-14 23:16:51 -0700

    completed dice app

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:dicee/styled_text.dart';
+import 'package:dicee/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({super.key, required this.gradientColors});
+  const GradientContainer({super.key, required this.gradientColors});
 
   final List<Color> gradientColors;
-  var activeDiceImage = 'assets/images/dice-1.png';
 
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -22,22 +20,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Image.asset(
-            activeDiceImage,
-            width: 200,
-          ),
-          const SizedBox(height: 20),
-          TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(fontSize: 28),
-            ),
-            child: const Text('Feelin Lucky!'),
-          ),
-        ]),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
